26a5e177524074e3467833b26b302a923c1395b9
PhamXuanLam <xuanlam21062002@gmail.com>
2024-03-25 19:02:15 +0700

    config cros

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // Route::post("register",[AuthController::class, "register"]);
+
 Route::post("login", [AuthController::class, "login"]);
 
 Route::group([
