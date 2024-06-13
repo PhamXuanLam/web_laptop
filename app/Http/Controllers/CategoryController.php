06812c08a349dd60fba5c4364a12f53c77488bde
PhamXuanLam <xuanlam21062002@gmail.com>
2024-06-13 15:35:34 +0700

    a

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Service\CategoryService;
+use App\Service\ProductService;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -15,8 +16,15 @@ class CategoryController extends Controller
 
     public function show(int $category_id) {
         $category = app(CategoryService::class)->getCategory($category_id);
+        $response = [];
+        foreach($category->products as $product) {
+            $productData = app(ProductService::class)->getProductById($product->id);
+            unset($productData['category']);
 
-        return response()->json($category->products);
+            $response[] = $productData;
+        }
+
+        return response()->json($response);
     }
 
     public function brands(int $category_id) {
